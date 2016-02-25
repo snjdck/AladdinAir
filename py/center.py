101e1f936a8c49126c93ed7740b912ad014de680
Alex Shao <shao_kai@qq.com>
2016-02-25 10:55:14 +0800

    update

--- py/center.py
+++ py/center.py
@@ -3,42 +3,34 @@ from python_modules import *
 HOST = "127.0.0.1"
 PORT = 7410
 
-class Dispatcher:
-	def __init__(self):
-		self.lock = allocate_lock()
-		self.notifyDict = {}
-		self.socketDict = {}
+lock = allocate_lock()
+notifyDict = {}
+socketDict = {}
 
-	def dispatch(self, sock, packet):
-		if sock not in self.socketDict:
-			self.socketDict[sock] = packet[2:].decode()
-			return
-		msgId = read_ushort(packet, 2)
-		if msgId not in self.notifyDict:
-			return
-		self.lock.acquire()
-		self.send(msgId, packet)
-		self.lock.release()
+notifyDict[0] = ["test", "gate"]
 
-	def send(self, msgId, packet):
-		handlerList = self.notifyDict[msgId]
-		for sock in self.socketDict.copy():
-			if self.socketDict[sock] not in handlerList:
+def dispatch(sock, packet):
+	if sock not in socketDict:
+		socketDict[sock] = packet[2:].decode()
+		return
+	msgId = read_ushort(packet, 2)
+	if msgId not in notifyDict:
+		return
+	handlerList = notifyDict[msgId]
+	with lock:
+		for sock in socketDict.copy():
+			if socketDict[sock] not in handlerList:
 				continue
 			try:
 				sock.sendall(packet)
 			except ConnectionResetError:
-				del self.socketDict[sock]
-
+				del socketDict[sock]
 
 def client_loop(sock, address):
-	read_sock_forever(sock, dispatcher.dispatch)
-
-dispatcher = Dispatcher()
-
-dispatcher.notifyDict[0] = ["test", "gate"]
+	read_sock_forever(sock, dispatch)
+	with lock:
+		del socketDict[sock]
 
 server = create_server(HOST, PORT)
-
 while True:
 	start_new_thread(client_loop, server.accept())
