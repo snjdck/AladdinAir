925bfb1c8bab6b3f81864bb359fa7a72a3e58c54
Alex Shao <shao_kai@qq.com>
2016-03-22 17:05:06 +0800

    update

--- py/ApacheServer.js
+++ py/ApacheServer.js
@@ -6,34 +6,26 @@ const fs = require("fs");
 const path = require("path");
 const url = require("url");
 
-var docRoot = "E:/test/bin-debug";
-docRoot = "D:/Program Files (x86)/Apache Software Foundation/Apache2.2/htdocs"
+const DOC_ROOT = "E:/test/bin-debug";
+const PORT = 8888;
 
-const server = new http.Server();
-server.listen(8888);
-
-server.on("request", function(request, response){
+http.createServer(function(request, response){
 	console.log(request.url);
 	const extName = path.extname(request.url).slice(1);
 	const pathname = url.parse(request.url).pathname;
-	const filePath = docRoot + pathname;
+	const filePath = DOC_ROOT + pathname;
 	fs.readFile(filePath, function(err, data){
 		if(err != null){
-			response.endWithError(404);
-			return;
+			response.statusCode = 404;
+		}else{
+			response.setHeader('Content-Type', findContentType(extName));
+			response.setHeader('Content-Length', data.length);
+			response.write(data);
 		}
-		response.setHeader('Content-Type', findContentType(extName));
-		response.setHeader('Content-Length', data.length);
-		response.statusCode = 200;
-		response.end(data);
+		response.end();
 	});
-});
+}).listen(PORT);
 
 function findContentType(extName){
 	return (extName in mime) ? mime[extName] : "unknow";
-}
-
-http.ServerResponse.prototype.endWithError = function(errorCode){
-	this.statusCode = errorCode;
-	this.end();
 }
