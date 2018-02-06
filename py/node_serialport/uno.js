756cc297c8d14231baab69f63a6437def9b1048a
Alex Shao <shao_kai@qq.com>
2018-02-06 11:47:26 +0800

    update

--- py/node_serialport/uno.js
+++ py/node_serialport/uno.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const SerialPort = require("./SerialPort");
 const fs = require("fs");
 
@@ -5,31 +7,31 @@ function sleep(seconds){
 	return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 }
 
-function waitResponse(target){
+function waitResponse(target, bytesWait){
 	return new Promise((resolve, reject) => {
-		var buffer = Buffer.alloc(0);
+		let buffer = Buffer.alloc(0);
 		target.on("data", data => {
 			buffer = Buffer.concat([buffer, data]);
-			if(buffer.length < 2){
+			if(buffer.length < bytesWait + 2){
 				return;
 			}
-			if(buffer[0] != 0x14 || buffer[1] != 0x10){
+			if(buffer[0] != 0x14 || buffer[bytesWait+1] != 0x10){
 				target.close();
 				reject();
 			}else{
 				target.removeAllListeners();
-				resolve();
+				resolve(buffer.slice(1, 1+bytesWait));
 			}
 		});
 	});
 }
 
-async function send(target, data){
+async function send(target, data, bytesWait=0){
 	target.write(data);
-	await waitResponse(target);
+	return await waitResponse(target, bytesWait);
 }
 
-function upload(port, payload){
+function upload(port, payload, uploadProgress=emptyFn, verifyProgress=emptyFn){
 	return new Promise((resolve, reject) => {
 		new SerialPort(port, {bitrate: 115200}, async function(error){
 			if(error != null){
@@ -39,43 +41,73 @@ function upload(port, payload){
 			await sleep(0.1);
 			await send(this, Buffer.from([0x30, 0x20]));
 			await send(this, Buffer.from([0x50, 0x20]));
-
-			var address = 0;
-			var addressInfo = Buffer.from([0x55, 0, 0, 0x20]);
-
-			while(address < payload.length){
-				var bytesSend = Math.min(0x80, payload.length - address);
-				addressInfo[1] = (address >> 1) & 0xFF;
-				addressInfo[2] = (address >> 9);
-				await send(this, addressInfo);
-				await send(this, Buffer.concat([
-					Buffer.from([0x64, 0x00, bytesSend, 0x46]),
-					payload.slice(address, address + bytesSend),
-					Buffer.from([0x20])
-				]));
-				address += bytesSend;
-			}
-
+			await writeData(this, payload, uploadProgress);
+			let verify = await readData(this, payload, verifyProgress);
 			await send(this, Buffer.from([0x51, 0x20]));
 
 			this.close();
-			resolve();
+
+			if(verify == 0){
+				resolve();
+			}else{
+				reject("verify failed!");
+			}
 		});
 	});
 }
 
-function uploadHex(port, data){
+const addressInfo = Buffer.from([0x55, 0, 0, 0x20]);
+const emptyFn = ()=>{};
+
+async function writeData(target, payload, onProgress){
+	const total = payload.length;
+	let address = 0;
+	while(address < total){
+		onProgress(address / total);
+		let bytesSend = Math.min(0x80, total - address);
+		addressInfo[1] = (address >> 1) & 0xFF;
+		addressInfo[2] = (address >> 9);
+		await send(target, addressInfo);
+		await send(target, Buffer.concat([
+			Buffer.from([0x64, 0x00, bytesSend, 0x46]),
+			payload.slice(address, address + bytesSend),
+			Buffer.from([0x20])
+		]));
+		address += bytesSend;
+	}
+	onProgress(1);
+}
+
+async function readData(target, payload, onProgress){
+	let buffer = Buffer.alloc(0);
+	const total = payload.length;
+	let address = 0;
+	while(address < total){
+		onProgress(address / total);
+		let bytesSend = 0x80;
+		addressInfo[1] = (address >> 1) & 0xFF;
+		addressInfo[2] = (address >> 9);
+		await send(target, addressInfo);
+		let data = await send(target, Buffer.from([0x74, 0x00, bytesSend, 0x46, 0x20]), bytesSend);
+		address += bytesSend;
+		buffer = Buffer.concat([buffer, data]);
+	}
+	onProgress(1);
+	return payload.compare(buffer, 0, total);
+}
+
+function uploadHex(port, data, uploadProgress, verifyProgress){
 	return upload(port, Buffer.from(data.trim()
 		.split(/\s+/)
 		.map(line => line.slice(9, -2))
 		.join("")
 		.match(/\w{2}/g)
 		.map(item => parseInt(item, 16))
-	));
+	), uploadProgress, verifyProgress);
 }
 
-function uploadFile(port, file){
-	return uploadHex(port, fs.readFileSync(file, "ascii"));
+function uploadFile(port, file, uploadProgress, verifyProgress){
+	return uploadHex(port, fs.readFileSync(file, "ascii"), uploadProgress, verifyProgress);
 }
 
 //[Colon] [Data Size] [Start Address] [Record Type] [Data] [Checksum]
